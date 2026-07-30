b7878fd771411af23d84f9c7c3f8a9c940da80a7
brandoncastillodev <brandoncastillo.09@gmail.com>
2026-07-30 10:29:05 -0300

    refactor: switch HashRouter to BrowserRouter, add vercel.json for SPA fallback

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { ReactNotifications } from "react-notifications-component";
 import { Provider } from "react-redux";
 import { Analytics } from "@vercel/analytics/react";
@@ -12,7 +12,7 @@ import "./styles/App.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <ReactNotifications className="notis" />
         <App />
         <Analytics />
